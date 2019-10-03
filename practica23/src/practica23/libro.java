4e3ff4b13d80c2b062d69f2bf1cdcea4a433d998
18progb0404 <18progb0404@TS-LABS-3.labs.cbtis94.local>
2019-10-03 13:50:38 -0500

    Se crea la clase libro, los contructores los get y set y el to string

--- practica23/src/practica23/libro.java
+++ practica23/src/practica23/libro.java
@@ -6,5 +6,59 @@ package practica23;
  * @author 18progb0404
  */
 public class libro {
+    private String titulo;
+    private String autor;
+    private int ejemplaresenexistenxia;
+    private int ejeplaresprestados;
+    
+    // crear los contructores
+
+    public libro() {
+    }
+
+    public libro(String titulo, String autor, int ejemplaresenexistenxia, int ejeplaresprestados) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ejemplaresenexistenxia = ejemplaresenexistenxia;
+        this.ejeplaresprestados = ejeplaresprestados;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getEjemplaresenexistenxia() {
+        return ejemplaresenexistenxia;
+    }
+
+    public void setEjemplaresenexistenxia(int ejemplaresenexistenxia) {
+        this.ejemplaresenexistenxia = ejemplaresenexistenxia;
+    }
+
+    public int getEjeplaresprestados() {
+        return ejeplaresprestados;
+    }
+
+    public void setEjeplaresprestados(int ejeplaresprestados) {
+        this.ejeplaresprestados = ejeplaresprestados;
+    }
+
+    @Override
+    public String toString() {
+        return "libro{" + "titulo=" + titulo + ", autor=" + autor + ", ejemplaresenexistenxia=" + ejemplaresenexistenxia + ", ejeplaresprestados=" + ejeplaresprestados + '}';
+    }
+    
     
 }
